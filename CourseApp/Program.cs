5e3443773c723ab5b261a36566fca7f601281b76
MrColteR <MrColteR@mail.ru>
2020-10-08 20:28:03 +0300

    My First Math Lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,33 +4,25 @@ namespace CourseApp
 {
     public class Program
     {
-        public static void TaskA(double a, double b)
+        public static void TaskA(double a, double b, double xStart, double xEnd, double xd)
         {
-            double y;
-            double numerator;
-            double denominator;
-            Console.WriteLine("------- TASK A -------");
-            for (double x = 0.08; x <= 1.08; x += 0.2)
+            for (xStart = 0.08; xStart <= xEnd; xStart += xd)
             {
-                numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(x, 2)), a);
-                denominator = Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 1 / 5.0);
-                y = numerator / denominator;
-                Console.WriteLine($"y =  {y}, x =  {x}");
+                double numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(xStart, 2)), a);
+                double denominator = Math.Pow(Math.Abs(Math.Pow(xStart, 2) - Math.Pow(a, 2)), 1 / 5.0);
+                double y = numerator / denominator;
+                Console.WriteLine($"y =  {y}, x =  {xStart}");
             }
         }
 
         public static void TaskB(double a, double b)
         {
-            Console.WriteLine("------- TASK B -------");
-            double y;
-            double numerator;
-            double denominator;
             double[] mas = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
             for (int i = 0; i < mas.Length; i++)
             {
-                numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(mas[i], 2)), a);
-                denominator = Math.Pow(Math.Abs(Math.Pow(mas[i], 2) - Math.Pow(a, 2)), 1 / 5.0);
-                y = numerator / denominator;
+                double numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(mas[i], 2)), a);
+                double denominator = Math.Pow(Math.Abs(Math.Pow(mas[i], 2) - Math.Pow(a, 2)), 1 / 5.0);
+                double y = numerator / denominator;
                 Console.WriteLine($"y =  {y}, x =  {mas[i]}");
             }
         }
@@ -39,7 +31,12 @@ namespace CourseApp
         {
             double a = 2.0;
             double b = 1.1;
-            TaskA(a, b);
+            double xStart = 0.08;
+            double xEnd = 1.08;
+            double xd = 0.2;
+            Console.WriteLine("------- TASK A -------");
+            TaskA(a, b, xStart, xEnd, xd);
+            Console.WriteLine("------- TASK B -------");
             TaskB(a, b);
         }
     }
