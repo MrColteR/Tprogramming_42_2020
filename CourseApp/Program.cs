c745f7f110d70d20ba0aa50a7034d97b1a8d2167
MrColteR <MrColteR@mail.ru>
2020-10-08 22:00:37 +0300

    My First Math Lab 3

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,40 +4,65 @@ namespace CourseApp
 {
     public class Program
     {
-        public static void TaskA(double a, double b, double xStart, double xEnd, double xd)
+        public static double Calc(double a, double b, double x)
         {
-            for (xStart = 0.08; xStart <= xEnd; xStart += xd)
-            {
-                double numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(xStart, 2)), a);
-                double denominator = Math.Pow(Math.Abs(Math.Pow(xStart, 2) - Math.Pow(a, 2)), 1 / 5.0);
-                double y = numerator / denominator;
-                Console.WriteLine($"y =  {y}, x =  {xStart}");
-            }
+            var numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(x, 2)), a);
+            var denominator = Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 1 / 5.0);
+            var y = numerator / denominator;
+            return y;
         }
 
-        public static void TaskB(double a, double b)
+        public static (double x, double y)[] TaskA(double a, double b, double xn, double xk, double dx)
         {
-            double[] mas = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
-            for (int i = 0; i < mas.Length; i++)
+            var res = new(double, double)[(int)Math.Ceiling((xk - xn) / dx) + 1];
+            int i = 0;
+            for (var x = xn; x <= xk; x += dx)
             {
-                double numerator = Math.Log(Math.Abs(Math.Pow(b, 2) - Math.Pow(mas[i], 2)), a);
-                double denominator = Math.Pow(Math.Abs(Math.Pow(mas[i], 2) - Math.Pow(a, 2)), 1 / 5.0);
-                double y = numerator / denominator;
-                Console.WriteLine($"y =  {y}, x =  {mas[i]}");
+                var y = Calc(a, b, x);
+                res[i] = (x, y);
+                i++;
             }
+
+            return res;
+        }
+
+        public static (double x, double y)[] TaskB(double a, double b, double[] xItems)
+        {
+            var res = new(double, double)[xItems.Length];
+            int i = 0;
+            foreach (var x in xItems)
+            {
+                var y = Calc(a, b, x);
+                res[i] = (x, y);
+                i++;
+            }
+
+            return res;
         }
 
         public static void Main(string[] args)
         {
-            double a = 2.0;
-            double b = 1.1;
-            double xStart = 0.08;
-            double xEnd = 1.08;
-            double xd = 0.2;
-            Console.WriteLine("------- TASK A -------");
-            TaskA(a, b, xStart, xEnd, xd);
-            Console.WriteLine("------- TASK B -------");
-            TaskB(a, b);
+            const double a = 2.0;
+            const double b = 1.1;
+            Console.WriteLine($"--------- TASK A --------------");
+            var taskA = TaskA(a, b, 0.8, 1.08, 0.2);
+            foreach (var item in taskA)
+            {
+                var(x, y) = item;
+                Console.WriteLine($"x={x}, y={y}");
+            }
+
+            Console.WriteLine($"--------- TASK B --------------");
+            double[] xItems = { 0.1, 1.3, 0.4, 0.45, 0.65 };
+            var taskB = TaskB(a, b, xItems);
+            foreach (var item in taskB)
+            {
+                var(x, y) = item;
+                Console.WriteLine($"x={x}, y={y}");
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
