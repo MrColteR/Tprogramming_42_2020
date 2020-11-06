c87c4a229b62117a6b1d2147af5832eeef42f507
MrColteR <MrColteR@mail.ru>
2020-11-06 16:15:33 +0300

    class

--- CourseApp/Country.cs
+++ CourseApp/Country.cs
@@ -10,9 +10,7 @@ namespace CourseApp
 
         public Country()
         {
-            this.population = 0;
-            this.area = 0;
-            this.density = 0;
+            Console.WriteLine($"Население, площадь и плоность равны 0");
         }
 
         public Country(int population, int area, double density)
@@ -24,22 +22,50 @@ namespace CourseApp
 
         public void AllInfo()
         {
-            Console.WriteLine($"Насление: {this.population} , площадь: {this.area} , плотность насление: {this.density} ");
+            if (this.population != 0 || this.area != 0 || this.density != 0)
+            {
+                Console.WriteLine($"Насление: {this.population} , площадь: {this.area} , плотность: {this.density} ");
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка ввода данных");
+            }
         }
 
         public void PopulationInfo()
         {
-            Console.WriteLine($"Население: {this.population}");
+            if (this.population != 0)
+            {
+                Console.WriteLine($"Население: {this.population}");
+            }
+            else
+            {
+                Console.WriteLine($"Население равна 0");
+            }
         }
 
         public void AreaInfo()
         {
-            Console.WriteLine($"Площадь: {this.area}");
+            if (this.area != 0)
+            {
+                Console.WriteLine($"Площадь: {this.area}");
+            }
+            else
+            {
+                Console.WriteLine($"Площадь равна 0");
+            }
         }
 
         public void DensityInfo()
         {
-            Console.WriteLine($"Плотность населения: {this.density}");
+            if (this.density != 0)
+            {
+                Console.WriteLine($"Плотность населения: {this.density}");
+            }
+            else
+            {
+                Console.WriteLine($"Плотность равна 0");
+            }
         }
     }
 }
