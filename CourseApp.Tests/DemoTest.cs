b3dbb30ae7e1c9889953f5865786111e8be0b1a0
MrColteR <MrColteR@mail.ru>
2020-11-11 23:54:58 +0300

    class tests

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -91,5 +91,29 @@ namespace CourseApp.Tests
             var actualResult = Program.CubeRoot(32);
             Assert.Equal(2, actualResult);
         }
+
+        [Fact]
+        public void ZeroOfPopulation()
+        {
+            Country country2 = new Country(0, 40, 185);
+            var actualResult = country2.Population;
+            Assert.Equal(0, actualResult);
+        }
+
+        [Fact]
+        public void ZeroOfArea()
+        {
+            Country country2 = new Country(13860, 0, 185);
+            var actualResult = country2.Area;
+            Assert.Equal(0, actualResult);
+        }
+
+        [Fact]
+        public void ZeroOfDensity()
+        {
+            Country country2 = new Country(13860, 40, 0);
+            var actualResult = country2.Density;
+            Assert.Equal(0, actualResult);
+        }
     }
 }
