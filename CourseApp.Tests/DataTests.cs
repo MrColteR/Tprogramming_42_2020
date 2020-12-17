6d33e72aa488c1a6de43d3e2c2bc35c9044fa64e
MrColteR <MrColteR@mail.ru>
2020-12-17 14:36:48 +0300

    data

--- CourseApp.Tests/DataTests.cs
+++ CourseApp.Tests/DataTests.cs
@@ -8,17 +8,35 @@ namespace CourseApp.Tests
         [Fact]
         public void DataAfter()
         {
-            DateTime a = new DateTime(20, 12, 12);
-            DateTime b = new DateTime(20, 12, 13);
+            DateTime a = new DateTime(2020, 12, 12);
+            DateTime b = new DateTime(2020, 12, 13);
             var actualResult = Program.Date(a, b);
             Assert.Equal("years = 0, months = 0, days = 1", actualResult);
         }
 
         [Fact]
+        public void Data1996()
+        {
+            DateTime a = new DateTime(1996, 12, 12);
+            DateTime b = new DateTime(2020, 12, 13);
+            var actualResult = Program.Date(a, b);
+            Assert.Equal("years = 24, months = 0, days = 1", actualResult);
+        }
+
+        [Fact]
+        public void Data2010()
+        {
+            DateTime a = new DateTime(2010, 12, 12);
+            DateTime b = new DateTime(2020, 12, 13);
+            var actualResult = Program.Date(a, b);
+            Assert.Equal("years = 10, months = 0, days = 1", actualResult);
+        }
+
+        [Fact]
         public void DataQuals()
         {
-            DateTime a = new DateTime(20, 12, 12);
-            DateTime b = new DateTime(20, 12, 12);
+            DateTime a = new DateTime(2020, 12, 12);
+            DateTime b = new DateTime(2020, 12, 12);
             var actualResult = Program.Date(a, b);
             Assert.Equal("years = 0, months = 0, days = 0", actualResult);
         }
@@ -26,8 +44,8 @@ namespace CourseApp.Tests
         [Fact]
         public void DataBefore()
         {
-            DateTime a = new DateTime(20, 12, 12);
-            DateTime b = new DateTime(20, 12, 11);
+            DateTime a = new DateTime(2020, 12, 12);
+            DateTime b = new DateTime(2020, 12, 11);
             var actualResult = Program.Date(a, b);
             Assert.Equal("years = 0, months = 0, days = -1", actualResult);
         }
